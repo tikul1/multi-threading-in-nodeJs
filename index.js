7192d2a01ef0463d7992fd9c80ff4185ed96ac6d
Hardik Parmar <hardik.parmar5@gmail.com>
2022-11-22 16:56:45 +0530

    using 4 worker thread to boost perofrmance of node js app

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const { Worker } = require("worker_threads");
+const THREAD_COUNT = 4;
+/*
+[5] now creating a new function to create worker_threads as pr the load of request.
+*/
+
+function createWorker() {
+  return new Promise((resolve, reject) => {
+    const worker = new Worker("./workerThreadIndex");
+    worker.on("message", (data) => {
+      resolve(data);
+    });
+    worker.on("error", (err) => {
+      reject(err);
+    });
+  });
+}
 
 /*
 [1] a non-blocking page it will be run instantly
@@ -32,6 +48,9 @@ app.get("/non-blocking/", (req, res) => {
 /*
 [2] a blocking page which will take some time to execute.
     as it has complex calculation within a for loop
+    ==> removed line 54 to 57
+    ==> removed line 59 for function
+    ==> removed line 61 to 68 to use function for worker thread
 */
 app.get("/blocking", async (req, res) => {
   //   let counter = 0;
@@ -39,15 +58,24 @@ app.get("/blocking", async (req, res) => {
   //      counter++;
   //   }
   //   const counter = await calculation();
-
-  const worker = new Worker("./workerThreadIndex");
-  worker.on("message", (data) => {
-    res.status(200).send(`result is ${data}`);
-  });
-  worker.on("error", (err) => {
-    res.status(401).send(`an error occured: ${err}`);
-  });
+  // //   const worker = new Worker("./workerThreadIndex");
+  // //   worker.on("message", (data) => {
+  // //     res.status(200).send(`result is ${data}`);
+  // //   });
+  // //   worker.on("error", (err) => {
+  // //     res.status(401).send(`an error occured: ${err}`);
+  //   });
   //   res.status(200).send(`result is ${counter}`);
+
+  const workerPromises = [];
+  for (let i = 0; i < THREAD_COUNT; i++) {
+    workerPromises.push(createWorker());
+  }
+
+  const thread_results = await Promise.all(workerPromises);
+  const total = thread_results[0] + thread_results[1] + thread_results[2] + thread_results[3];
+  console.log(thread_results[0]);
+  res.status(200).send(`result is ${total}`);
 });
 
 /*
--- workerThreadIndex.js
+++ workerThreadIndex.js
@@ -2,9 +2,10 @@ const { parentPort } = require("worker_threads");
 //
 //[1] praentPort is class is used to send msg to the main thread when completion of task.
 //[2] we will use same calculation which we will remove from index file.
-//
+//[3] workerdata is to import worker threads ==> workerData.thread_count. which is 4 in my system
+//[4] so 20 billion divided by 4. so our performance will be 4 times better than before.
 let counter = 0;
-for (let i = 0; i < 20_000_000_000; i++) {
+for (let i = 0; i < 20_000_000_000 / 4; i++) {
   counter++;
 }
 
