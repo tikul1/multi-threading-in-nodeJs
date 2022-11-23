4449271f5453811fcfc7f014ab6b954fc24b3ebd
Hardik Parmar <hardik.parmar5@gmail.com>
2022-11-23 18:03:43 +0530

    added info

--- clusterIndex.js
+++ clusterIndex.js
@@ -8,6 +8,9 @@ const cpuCount = require("os").cpus().length;
 
 /*
 creating new functions to create clusters
+[1] in if condition. it will be masterprocess so it will create new 4 clusster
+[2] cluster fork is to create cluster and exit is when cluster is died it will create a new cluster.
+[3] now in else all 4 are child process. so it will create new app in all 4 clusters dividing load by 4.
 */
 
 //to check cluster
@@ -29,6 +32,7 @@ function masterProcess() {
     cluster.fork();
   });
 }
+
 function childProcess() {
   const express = require("express");
   const app = express();
